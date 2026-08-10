0eddf560953067d941c05ec06cfaba9c0870d010
manthan-sagar <manthan2007sagar@gmail.com>
2026-08-10 22:47:01 +0530

    error handling part 1 , before controller

---
+++ src/main/java/com/manthan/restraunt/domain/dtos/ErrorDto.java
@@ -0,0 +1,15 @@
+package com.manthan.restraunt.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorDto {
+    private Integer status;
+    private String message;
+}
